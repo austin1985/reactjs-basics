6a08995d1efa4c885753c6fc625e7a997e3c6cf8
austin1985 <tigerinformation00@gmail.com>
2018-10-03 21:53:15 +0200

    on button click write to console added

--- src/App.js
+++ src/App.js
@@ -14,11 +14,18 @@ class App extends Component {
 
   }
 
+  //handling the button press
+  switchNameHandler = () => {
+
+    console.log("Was clicked!");
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi im a react app!</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: writing, learning.</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
