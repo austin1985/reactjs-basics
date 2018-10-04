22c995468c8dff641e570831dac1099e6c09f052
austin1985 <tigerinformation00@gmail.com>
2018-10-04 20:25:22 +0200

    Binding inputs to props and state , function reference passed on via props

--- src/App.js
+++ src/App.js
@@ -15,20 +15,52 @@ class App extends Component {
   }
 
   //handling the button press
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
+    //console.log("Was clicked!");
+    this.setState({ 
+    
+      persons: [
+        { name: newName, age:28 },
+        { name: 'Mike',age:33 },
+        { name: 'Stephanie', age:30 }
+      ]
+  
+    
+    });
+  }
 
-    console.log("Was clicked!");
+  nameChangeHandler = (event) => {
+
+    this.setState({ 
+    
+      persons: [
+        { name: 'Max', age:28 },
+        { name: event.target.value, age:33 },
+        { name: 'Stephanie', age:30 }
+      ]
+  
+    
+    });
 
   }
+
 
   render() {
     return (
       <div className="App">
         <h1>Hi im a react app!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: writing, learning.</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={()=> this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this,'Max')}
+          changed={this.nameChangeHandler}>Hobbies: writing, learning.</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi iam a react App!!!'));
