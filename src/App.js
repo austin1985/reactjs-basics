44880c87db4266352ccb7b7908edd7f3e4d746ae
austin1985 <tigerinformation00@gmail.com>
2018-10-14 12:53:05 +0200

    ssadas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+
 class App extends Component {
 
   state = {
@@ -62,40 +63,57 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
 
     };
 
     let persons = null;
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+
+      classes.push('red');
+
+    }
+
+    if(this.state.persons.length <= 1){
+
+      classes.push('bold');
+
+    }
 
     if (this.state.showPersons) {
 
       persons = (
         <div>
           {this.state.persons.map((person,index) =>{
-
+              
               return <Person 
               click={()=>this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
               key={person.id}
               changed={(event)=>this.nameChangeHandler(event, person.id)}/>
-
+              
           })}
 
       </div>
       );
+
       style.backgroundColor = 'red';
+
     }
 
     return (
+
       <div className="App">
         <h1>Hi im a react app!</h1>
+        <p className={classes.join(' ')}>This is working</p>
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Toggle names</button>
         {persons}
       </div>
+     
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi iam a react App!!!'));
   }
