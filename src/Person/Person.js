cc72427f0d2a96e509df9a98e2b048d2e13b3559
austin1985 <tigerinformation00@gmail.com>
2018-10-14 13:18:31 +0200

    pre css module state

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,15 +3,9 @@ import './Person.css';
 import Radium from 'radium';
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)' : {
-
-            width: '450px'
-        }
-    };
-
+    
     return (
-    <div className="Person" style={style}>
+    <div className="Person" >
         <p onClick={props.click}>Im {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
